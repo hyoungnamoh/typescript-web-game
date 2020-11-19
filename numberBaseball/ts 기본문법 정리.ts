58e969210b4fe4f9c9d6daa672437ba549fcf74d
오형남 <zzzsh789@naver.com>
2020-11-19 22:39:22 +0900

    누락된 커밋

--- numberBaseball/ts 기본문법 정리.ts
+++ numberBaseball/ts 기본문법 정리.ts
@@ -63,3 +63,46 @@ interface aa {
 
 // interface
 // 객체의 타입을 인터페이스를 둠
+// 주로 객체에 많이 사용
+// 같은 이름 여러개로 분리할 수 있음 컴파일 시 모두 합쳐짐
+interface RSP {
+  readonly ROCK: '0',
+  readonly SCISSORS: '-142px',
+  readonly PAPER: '-284px',
+}
+
+interface Example { //뭐가 들어올지 모를 때 이렇게 널널하게 줄 수 있음
+  a: 3,
+  b: 7,
+  [key: string]: number,
+}
+const ex: Example = {
+  a: 3,
+  b: 7,
+}
+const ex2: Example = {
+  a: 3,
+  b: 7,
+  dasdd: 12514124123,
+}
+
+// type alias
+// interface 보다 좀 더 넓은 범위
+// 커스텀 타입같은 느낌?
+// | 많이 씀
+// 객체는 웬만하면 interface로, 타입 여러개 만들면 복잡해짐
+type Hello = string | number;
+type Hello2 = string | {
+  ROCK: string
+  SCISSORS: string
+  PAPER: string
+}
+const hi: Hello2 = {
+  ROCK: '0',
+  SCISSORS: '-142px',
+  PAPER: 'ze',
+}
+const hi2: Hello2 = 'hello2';
+
+// keyof interface명 === interface 키값들을 가져옴
+// interface명[keyof interface명] === interface 값들을 가져올 수 있음
